52ad60c587f19df90a5d4fa31b5b6c29f77b1ac1
Hovo Serobyan <129414253+Hovhannes09@users.noreply.github.com>
2026-06-29 19:28:18 +0400

    Server created

---
+++ app.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import path from "path";
+import morgan from "morgan";
+import express from "express";
+import { createServer } from "http";
+
+const app = express();
+
+const { PORT } = process.env;
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
